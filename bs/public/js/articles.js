04d4dfbdb73c20a0391f33e097fa1ab0e02b6b9e
lqq <294779867@qq.com>
2017-04-17 11:28:28 +0800

    完善了分类功能

--- bs/public/js/articles.js
+++ bs/public/js/articles.js
@@ -27,10 +27,12 @@ $(function() {
     var category = parseInt(get_query("category")) || 0;
     var skip_num = parseInt(get_query("skip_num")) || 0;
     $.get("/findPublish", { "articles": category ? { "category": category } : {}, "limit": 10, "skip_num": skip_num }, function(result) {
-        if (result == "-1") {
+        var result2 = JSON.stringify(result);
+        result2 = JSON.parse(result2);
+        if (result2 == "-1") {
             return;
         }
-        for (var i = 0, length = result.invitation.length; i < length; ++i) {
+        for (var i = 0, length = result2.invitation.length; i < length; ++i) {
             var compiled = _.template('\
                     <article class="format-standard type-post hentry clearfix">\
                         <header class="clearfix">\
@@ -39,7 +41,7 @@ $(function() {
                             </h3>\
                             <div class="post-meta clearfix">\
                                 <span class="date">{{=date}}</span>\
-                                <span class="category"><a href="#" title="View all posts in Server &amp; Database">{{=category}}</a></span>\
+                                <span class="category"><a href="articles?category={{=category}}&skip_num=0" title="View all posts in Server &amp; Database">{{=category_formit}}</a></span>\
                                 <span class="comments"><a href="#" title="Comment on Integrating WordPress with Your Website">{{=reply_num}}</a></span>\
                                 <span class="like-count">{{=all_praise}}</span>\
                             </div>\
@@ -47,9 +49,9 @@ $(function() {
                         <p>{{message}}<a class="readmore-link" href="">Read more</a></p>\
                     </article>\
             ');
-            result.invitation[i].category = get_category(result.invitation[i].category);
-            result.invitation[i].date = get_localTime(result.invitation[i].date);
-            var compiled = compiled(result.invitation[i]);
+            result2.invitation[i].category_formit = get_category(result2.invitation[i].category);
+            result2.invitation[i].date = get_localTime(result2.invitation[i].date);
+            var compiled = compiled(result2.invitation[i]);
             $("#articles").append(compiled);
         }
     })
--- bs/public/js/index.js
+++ bs/public/js/index.js
@@ -21,42 +21,48 @@ $(function() {
 
     //得到帖子
     $.get("/findPublish", { "limit_num": 5 }, function(result) {
-        if (result == "-1") {
+
+        var result2 = JSON.stringify(result);
+        result2 = JSON.parse(result2);
+
+        if (result2 == "-1") {
             return;
         }
-        for (var i = 0, length = result.invitation.length; i < length; ++i) {
+        for (var i = 0, length = result2.invitation.length; i < length; ++i) {
             var compiled = _.template('\
                 <li class="article-entry standard">\
                     <h4><a href="single?_id={{=_id}}">{{=title}}</a></h4>\
-                    <span class="article-meta">{{=date}} in <a href="#" title="View all posts in Server &amp; Database">{{=category}}</a></span>\
+                    <span class="article-meta">{{=date}} in <a href="/articles?category={{=category}}&skip_num=0" title="View all posts in Server &amp; Database">{{=category_formit}}</a></span>\
                     <span class="like-count">{{=all_praise}}</span>\
                 </li>\
             ');
-            result.invitation[i].category = get_category(result.invitation[i].category);
-            result.invitation[i].date = get_localTime(result.invitation[i].date);
+            result2.invitation[i].category_formit = get_category(result2.invitation[i].category);
+            result2.invitation[i].date = get_localTime(result2.invitation[i].date);
 
-            var compiled = compiled(result.invitation[i]);
+            var compiled = compiled(result2.invitation[i]);
             $(".latest_invitation").append(compiled);
         }
     })
 
     //得到热门帖子
     $.get("/findPublish", { "limit_num": 5, sort_data: { all_praise: -1 } }, function(result) {
-        if (result == "-1") {
+        var result2 = JSON.stringify(result);
+        result2 = JSON.parse(result2);
+        if (result2 == "-1") {
             return;
         }
-        for (var i = 0, length = result.invitation.length; i < length; ++i) {
+        for (var i = 0, length = result2.invitation.length; i < length; ++i) {
             var compiled = _.template('\
                 <li class="article-entry standard">\
                     <h4><a href="single?_id={{=_id}}">{{=title}}</a></h4>\
-                    <span class="article-meta">{{=date}} in <a href="#" title="View all posts in Server &amp; Database">{{=category}}</a></span>\
+                    <span class="article-meta">{{=date}} in <a href="/articles?category={{=category}}&skip_num=0" title="View all posts in Server &amp; Database">{{=category_formit}}</a></span>\
                     <span class="like-count">{{=all_praise}}</span>\
                 </li>\
             ');
-            result.invitation[i].category = get_category(result.invitation[i].category);
-            result.invitation[i].date = get_localTime(result.invitation[i].date);
+            result2.invitation[i].category_formit = get_category(result2.invitation[i].category);
+            result2.invitation[i].date = get_localTime(result2.invitation[i].date);
 
-            var compiled = compiled(result.invitation[i]);
+            var compiled = compiled(result2.invitation[i]);
             $(".hot_invitation").append(compiled);
         }
     })
